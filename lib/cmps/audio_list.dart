945f5799eb471299ea86ef3a6c453b502371ada9
Chuks Okwuenu <chuksjimy@gmail.com>
2021-06-26 12:18:05 +0100

    re-structure flow for v1

--- lib/cmps/audio_list.dart
+++ lib/cmps/audio_list.dart
@@ -34,94 +34,179 @@ class _AudioListsState extends State<AudioLists> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Container(
-        child: VsScrollbar(
-          controller: _scrollController,
-          scrollDirection: Axis.vertical, // @REQUIRED
-          allowDrag:
-              true, // allows to scroll the list using scrollbar [default : true]
-          color: Colors.green, // sets color of vsScrollBar
-          radius: 50, // sets radius of vsScrollBar
-          thickness: 5, // sets thickness of vsScrollBar
-          isAlwaysShown: true, // default false
-          // sets scrollbar fade animation duration [ Default : Duration(milliseconds: 300)]
-          scrollbarFadeDuration: Duration(milliseconds: 500),
-          // Fades scrollbar after certain duration [ Default : Duration(milliseconds: 600)]
-          scrollbarTimeToFade: Duration(milliseconds: 800),
-          child: ListView.builder(
-              controller: _scrollController,
-              itemCount: widget.songs.length,
-              itemBuilder: (context, index) {
-                return InkWell(
-                  focusColor: Colors.grey,
-                  onTap: () {
-                    CurrentIndex = index;
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => MusicPlayer(
-                              changeTrack: changeTrack,
-                              songInfo: widget.songs[CurrentIndex],
-                              key: key,
-                            )));
-                  },
-                  child: Padding(
-                    padding: EdgeInsets.fromLTRB(12, 10, 0, 2),
-                    child: Row(
-                      children: [
-                        Container(
-                          height: 50.0,
-                          width: 50.0,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10.0),
-                              image: DecorationImage(
-                                image: widget.songs[index].albumArtwork == null
-                                    ? AssetImage("assets/music_icon.jpg")
-                                    : File(widget.songs[index].albumArtwork)
-                                            .existsSync()
-                                        ? FileImage(File(
-                                            widget.songs[index].albumArtwork))
-                                        : AssetImage("assets/music_icon.jpg"),
-                              )),
-                        ),
-                        Expanded(
-                          child: Padding(
-                            padding:
-                                const EdgeInsets.fromLTRB(12.0, 8.0, 8.0, 8.0),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  widget.songs[index].title,
-                                  overflow: TextOverflow.ellipsis,
-                                  softWrap: true,
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                Text(
-                                  widget.songs[index].artist,
-                                  overflow: TextOverflow.ellipsis,
-                                  softWrap: true,
-                                  style: TextStyle(color: Colors.white60),
-                                )
-                              ],
-                            ),
+        child: ListView.builder(
+            controller: _scrollController,
+            itemCount: widget.songs.length,
+            shrinkWrap: true,
+            physics: BouncingScrollPhysics(),
+            scrollDirection: Axis.vertical,
+            itemBuilder: (context, index) {
+              return InkWell(
+                focusColor: Colors.grey,
+                onTap: () {
+                  CurrentIndex = index;
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => MusicPlayer(
+                            changeTrack: changeTrack,
+                            songInfo: widget.songs[CurrentIndex],
+                            key: key,
+                          )));
+                },
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(12, 10, 0, 2),
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 50.0,
+                        width: 50.0,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10.0),
+                            image: DecorationImage(
+                              image: widget.songs[index].albumArtwork == null
+                                  ? AssetImage("assets/music_icon.jpg")
+                                  : File(widget.songs[index].albumArtwork)
+                                          .existsSync()
+                                      ? FileImage(File(
+                                          widget.songs[index].albumArtwork))
+                                      : AssetImage("assets/music_icon.jpg"),
+                            )),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding:
+                              const EdgeInsets.fromLTRB(12.0, 8.0, 8.0, 8.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                widget.songs[index].title,
+                                overflow: TextOverflow.ellipsis,
+                                softWrap: true,
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                widget.songs[index].artist,
+                                overflow: TextOverflow.ellipsis,
+                                softWrap: true,
+                                style: TextStyle(color: Colors.white60),
+                              )
+                            ],
                           ),
                         ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(0, 0, 13.0, 8.0),
-                          child: Icon(
-                            Icons.more_vert,
-                            color: Colors.white,
-                          ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 13.0, 8.0),
+                        child: Icon(
+                          Icons.more_vert,
+                          color: Colors.white,
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                );
-              }
-              // childCount: widget.songs.length,
-              ),
-        ),
+                ),
+              );
+            }
+            // childCount: widget.songs.length,
+            ),
+        // child: VsScrollbar(
+        //   controller: _scrollController,
+        //   // scrollDirection: Axis.vertical, // @REQUIRED
+        //   // allowDrag:
+        //   //     true, // allows to scroll the list using scrollbar [default : true]
+        //   // color: Colors.green, // sets color of vsScrollBar
+        //   // radius: 50, // sets radius of vsScrollBar
+        //   // thickness: 5, // sets thickness of vsScrollBar
+        //   showTrackOnHover: true,
+        //   isAlwaysShown: true, // default false
+        //   scrollbarFadeDuration: Duration(milliseconds: 500),
+        //   scrollbarTimeToFade: Duration(milliseconds: 800),
+        //   style: VsScrollbarStyle(
+        //     hoverThickness: 5.0, // default 12.0
+        //     radius: Radius.circular(10), // default Radius.circular(8.0)
+        //     thickness: 5.0, // [ default 8.0 ]
+        //     color: Colors.green.shade900,
+        //   ),
+        //   child: ListView.builder(
+        //       controller: _scrollController,
+        //       itemCount: widget.songs.length,
+        //       shrinkWrap: true,
+        //       physics: BouncingScrollPhysics(),
+        //       scrollDirection: Axis.vertical,
+        //       itemBuilder: (context, index) {
+        //         return InkWell(
+        //           focusColor: Colors.grey,
+        //           onTap: () {
+        //             CurrentIndex = index;
+        //             Navigator.of(context).push(MaterialPageRoute(
+        //                 builder: (context) => MusicPlayer(
+        //                       changeTrack: changeTrack,
+        //                       songInfo: widget.songs[CurrentIndex],
+        //                       key: key,
+        //                     )));
+        //           },
+        //           child: Padding(
+        //             padding: EdgeInsets.fromLTRB(12, 10, 0, 2),
+        //             child: Row(
+        //               children: [
+        //                 Container(
+        //                   height: 50.0,
+        //                   width: 50.0,
+        //                   decoration: BoxDecoration(
+        //                       borderRadius: BorderRadius.circular(10.0),
+        //                       image: DecorationImage(
+        //                         image: widget.songs[index].albumArtwork == null
+        //                             ? AssetImage("assets/music_icon.jpg")
+        //                             : File(widget.songs[index].albumArtwork)
+        //                                     .existsSync()
+        //                                 ? FileImage(File(
+        //                                     widget.songs[index].albumArtwork))
+        //                                 : AssetImage("assets/music_icon.jpg"),
+        //                       )),
+        //                 ),
+        //                 Expanded(
+        //                   child: Padding(
+        //                     padding:
+        //                         const EdgeInsets.fromLTRB(12.0, 8.0, 8.0, 8.0),
+        //                     child: Column(
+        //                       mainAxisAlignment: MainAxisAlignment.start,
+        //                       crossAxisAlignment: CrossAxisAlignment.start,
+        //                       children: [
+        //                         Text(
+        //                           widget.songs[index].title,
+        //                           overflow: TextOverflow.ellipsis,
+        //                           softWrap: true,
+        //                           style: TextStyle(
+        //                               color: Colors.white,
+        //                               fontWeight: FontWeight.bold),
+        //                         ),
+        //                         Text(
+        //                           widget.songs[index].artist,
+        //                           overflow: TextOverflow.ellipsis,
+        //                           softWrap: true,
+        //                           style: TextStyle(color: Colors.white60),
+        //                         )
+        //                       ],
+        //                     ),
+        //                   ),
+        //                 ),
+        //                 Padding(
+        //                   padding: const EdgeInsets.fromLTRB(0, 0, 13.0, 8.0),
+        //                   child: Icon(
+        //                     Icons.more_vert,
+        //                     color: Colors.white,
+        //                   ),
+        //                 ),
+        //               ],
+        //             ),
+        //           ),
+        //         );
+        //       }
+        //       // childCount: widget.songs.length,
+        //       ),
+        // ),
       ),
     );
   }
